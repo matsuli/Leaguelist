f2f7da50ec826fdc6c865c3ef12e06aeb6c05be5
matsuli <nuts.hol@gmail.com>
2018-06-23 14:25:07 +0300

    search in appbar working

--- app/src/main/java/com/example/leaguelist/MainScreen.java
+++ app/src/main/java/com/example/leaguelist/MainScreen.java
@@ -1,8 +1,11 @@
 package com.example.leaguelist;
 
+import android.app.SearchManager;
+import android.content.Context;
 import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.SearchView;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.support.v7.widget.Toolbar;
@@ -20,12 +23,20 @@ public class MainScreen extends AppCompatActivity {
         Objects.requireNonNull(getSupportActionBar()).setTitle(null);
     }
 
-//    @Override
-//    public boolean onCreateOptionsMenu(Menu menu) {
-//        // Inflate the menu_leaguelist; this adds items to the action bar if it is present.
-//        getMenuInflater().inflate(R.menu.menu_leaguelist, menu);
-//        return true;
-//    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu_leaguelist; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_leaguelist, menu);
+
+        // Associate searchable configuration with the SearchView
+        SearchManager searchManager =
+                (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView =
+                (SearchView) menu.findItem(R.id.search).getActionView();
+        searchView.setSearchableInfo(
+                searchManager.getSearchableInfo(getComponentName()));
+        return true;
+    }
 
     // Determines if Action bar item was selected. If true then do corresponding action.
     @Override
